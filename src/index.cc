22ae2ec14415b015a8cf74503c82a147ca0581b2
Tomas Szabo <tomas.szabo@zeptura.com>
2019-09-20 17:25:09 +0200

    Updated to Electron 6.0.10

--- src/index.cc
+++ src/index.cc
@@ -5,12 +5,15 @@ void getDarkMode(const Nan::FunctionCallbackInfo<v8::Value>& info);
 void setDarkMode(const Nan::FunctionCallbackInfo<v8::Value>& info);
 
 static void InitModule(v8::Local<v8::Object> exports) {
-  exports->Set(Nan::New("getSystemDarkMode").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(getSystemDarkMode)->GetFunction());
-  exports->Set(Nan::New("getDarkMode").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(getDarkMode)->GetFunction());
-  exports->Set(Nan::New("setDarkMode").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(setDarkMode)->GetFunction());
+  v8::Isolate *isolate = exports->GetIsolate();
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  exports->Set(context, Nan::New("getSystemDarkMode").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(getSystemDarkMode)->GetFunction(context).ToLocalChecked());
+  exports->Set(context, Nan::New("getDarkMode").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(getDarkMode)->GetFunction(context).ToLocalChecked());
+  exports->Set(context, Nan::New("setDarkMode").ToLocalChecked(),
+               Nan::New<v8::FunctionTemplate>(setDarkMode)->GetFunction(context).ToLocalChecked());
 }
 
 NODE_MODULE(themed_window, InitModule)
